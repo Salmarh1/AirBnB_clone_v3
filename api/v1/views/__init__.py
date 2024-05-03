e80ee881aa3803424003dc866eec598b9bc770f1
Salmarh1 <salmarhadi5@gmail.com>
2024-05-03 21:18:21 +0000

    0x05. AirBnB clone - RESTful API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""This module defines a blueprint for routes with the Blueprint object"""
+"""
+views
+"""
+
 from flask import Blueprint
 
-# Import views
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
-from api.v1.views.cities import *
 from api.v1.views.amenities import *
-from api.v1.views.users import *
+from api.v1.views.cities import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from api.v1.views.users import *
 from api.v1.views.places_amenities import *
-
-# Define Blueprint
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
